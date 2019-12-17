025ce0467768822484caa351c9c2ce606145ce63
yuchen <373428582@qq.com>
2019-12-17 10:14:07 +0800

    update first

---
+++ js/index.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("你好ts");
+function getData() {
+    console.log(123);
+}
+var str = "你好";
+console.log(str);
---
+++ src/index/index.ts
@@ -0,0 +1,8 @@
+console.log("你好ts")
+function getData(){
+    console.log(123)
+}
+var str:string="你好"
+console.log(str)
+var num:number=123;
+console.log(num)
